ab2813a0e5814aca953cc809fb163aa14ed04d7c
Md Faizaan Alam <83642826+mdfaizaanalam@users.noreply.github.com>
2023-11-09 09:42:50 +0530

    Update Day1.java
    Update Day1.java

--- Day1.java
+++ Day1.java
@@ -5,9 +5,10 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
+
 public class Solution {
-    
     public static void main(String[] args) {
+        
         int i = 4;
         double d = 4.0;
         String s = "HackerRank ";
