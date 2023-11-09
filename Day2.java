2c5a3379d4b6c14b8dc5d0eea248e4196d69d0f8
Md Faizaan Alam <83642826+mdfaizaanalam@users.noreply.github.com>
2023-11-09 09:43:27 +0530

    Update Day2.java
    Update Day2.java

--- Day2.java
+++ Day2.java
@@ -7,8 +7,8 @@ import java.text.*;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
-public class Solution {
 
+public class Solution {
     // Complete the solve function below.
 static void solve(double meal_cost, int tip_percent, int tax_percent) {
         double tip=meal_cost*tip_percent/100;
