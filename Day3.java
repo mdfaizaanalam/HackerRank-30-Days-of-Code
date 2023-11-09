6107bbf5e2da25b2ddc57dee8d3bf121daaccc41
Md Faizaan Alam <83642826+mdfaizaanalam@users.noreply.github.com>
2023-11-09 09:44:03 +0530

    Update Day3.java
    Update Day3.java

--- Day3.java
+++ Day3.java
@@ -9,14 +9,12 @@ import java.util.concurrent.*;
 import java.util.regex.*;
 
 public class Solution {
-
     private static final Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
 
         int N = scanner.nextInt();
-
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        
     if(N%2==0){
         if(N>=2 && N<=5)
             System.out.print("Not Weird");
