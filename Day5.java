3fdbbdb2178487b4586f995040699af6221a34db
Md Faizaan Alam <83642826+mdfaizaanalam@users.noreply.github.com>
2023-11-09 09:22:02 +0530

    Update Day5.java
    Day5.java

--- Day5.java
+++ Day5.java
@@ -9,16 +9,13 @@ import java.util.concurrent.*;
 import java.util.regex.*;
 
 public class Solution {
-
     private static final Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
 
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
         
-        for (int i = 1; i <= 10; i++) 
-        {
+        for (int i = 1; i <= 10; i++) {
             System.out.println(n + " x " + i + " = " + n * i);
         }
         scanner.close();
